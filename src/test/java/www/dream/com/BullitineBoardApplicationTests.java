d015d691b2489ed2b97263eb64befa2b0f160da5
nasms018 <cjm1s@naver.com>
2023-09-26 10:33:48 +0900

    신규프로젝트로 재생성

---
+++ src/test/java/www/dream/com/BullitineBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package www.dream.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullitineBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
